92e668aa95ea31ab9001100fbc293ed0f24e3708
AleksaYO <113035091+AleksaYO@users.noreply.github.com>
2023-05-17 15:16:25 +0000

    Deploying to gh-pages from @ AleksaYO/goit-test@3fa3464ea4cc1a474eef977c077d288c7fc3a4b4 🚀

---
+++ static/js/687.21293b62.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[687],{687:function(e,r,t){t.r(r);var c=t(87),n=t(184);r.default=function(){return(0,n.jsxs)("div",{children:["return ",(0,n.jsx)(c.rU,{to:"/cards",children:"Cards"}),(0,n.jsx)("p",{children:"XYU"})]})}}}]);
+//# sourceMappingURL=687.21293b62.chunk.js.map
